5fe3e88b677c8311d98a372034c631859b1e597b
LorenzoRossini01 <lory.rox.01@gmail.com>
2024-04-12 17:00:55 +0200

    feat: create axios call to get project data

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,39 @@
 <script>
+import axios from "axios";
+
 export default {
   data() {
     return {
       title: "Boolfolio",
+      projects: [],
     };
+  },
+
+  methods: {
+    fetchProjects(endpoint = "http://127.0.0.1:8000/api/projects") {
+      axios.get(endpoint).then((response) => {
+        this.projects = response.data.data;
+      });
+    },
+  },
+
+  created() {
+    this.fetchProjects();
   },
 };
 </script>
 
 <template>
   <h1>{{ title }}</h1>
+  <div v-for="project in projects">
+    <ul>
+      <li><strong>title: </strong>{{ project.title }}</li>
+      <li><strong>description: </strong>{{ project.description }}</li>
+      <li><strong>link: </strong>{{ project.link }}</li>
+      <li><strong>img: </strong>{{ project.imageUrl }}</li>
+    </ul>
+    <hr />
+  </div>
 </template>
 
 <style lang="scss">
