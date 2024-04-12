b1c1c5d75d756ea1abb715f5819e7c12ceeb344c
LorenzoRossini01 <lory.rox.01@gmail.com>
2024-04-12 16:23:11 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script>
+export default {
+  data() {
+    return {
+      title: "Boolfolio",
+    };
+  },
+};
+</script>
+
+<template>
+  <h1>{{ title }}</h1>
+</template>
+
+<style lang="scss">
+@use "/src/scss/general.scss";
+</style>
